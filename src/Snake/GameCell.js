7582054ef6cb1d0420cbf000bc5d34347411ea26
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-11-30 10:54:12 +0100

    styling cells

--- src/Snake/GameCell.js
+++ src/Snake/GameCell.js
@@ -5,22 +5,32 @@ const style = {
     display: "flex",
     justifyContent: 'center',
     alignItems: "center",
-    flexBasis: "10%"
+    // flexBasis: "10%"
 }
 
 
-const GameCell = (props) => (
-    <div
-        style={{
-            ...style,
-        flexBasis: 100/props.numberOfCells+ '%'
-    }}
-    >
-        {props.cell}
-    </div>
+const GameCell = (props) => {
+    const color = (
+        props.cell === 0 ?
+            'black'
+            :
+            props.cell === 'F' ?
+                "green"
+                :
+                "white"
+    )
+    return (
+        <div
+            style={{
+                ...style,
+                flexBasis: 100 / props.numberOfCells + '%',
+                backgroundColor: color
+            }}
+        >
+        </div>
+    )
 
-
-)
+}
 
 
 export default GameCell
