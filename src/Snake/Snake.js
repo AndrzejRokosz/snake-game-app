a286dd27321552d462eeb6b5e2099e6ddd0251ce
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-11-30 11:50:46 +0100

    logic wip

--- src/Snake/Snake.js
+++ src/Snake/Snake.js
@@ -29,7 +29,8 @@ class Snake extends React.Component {
                 'right',
                 'left'
             ],
-            currentPlayerIndex:0
+            currentPlayerIndex:0,
+            gameTickTime:props.startGameTickTime
 
         }
     }
@@ -38,13 +39,52 @@ class Snake extends React.Component {
     }
     componentDidMount(){
             this.intervalId=setInterval(
-                this.gameTick,1000
-            )
+                this.gameTick,
+                this.state.gameTickTime)
     }
 
     componentWillUnmount(){
         clearInterval(this.intervalId)
     }
+    checkIfMovesAreAvailable=()=>{
+        this.state.snakes.forEach((snakePositions,i)=>{
+            const snakeHeadPosition=snakePositions[0]
+            const direction=this.state.directions[i]
+            let newSnakeHeadPosition=null
+            
+            switch(direction){
+                case 'left':
+                newSnakeHeadPosition={
+                    x:snakeHeadPosition.x-1,
+                    y:snakeHeadPosition.y
+                }
+                break
+                case 'right':
+                newSnakeHeadPosition={
+                    x:snakeHeadPosition.x+1,
+                    y:snakeHeadPosition.y
+                }
+                break
+                case 'top':
+                newSnakeHeadPosition={
+                    x:snakeHeadPosition.x,
+                    y:snakeHeadPosition.y-1
+                }
+                break
+                case 'bottom':
+                newSnakeHeadPosition={
+                    x:snakeHeadPosition.x,
+                    y:snakeHeadPosition.y+1
+                }
+                break
+                default:
+            }
+
+        })
+    }
+
+
+
     composeGameBoard = () => {
         const gameBoardCopy = JSON.parse(JSON.stringify(this.state.gameBoard))
 
@@ -79,7 +119,8 @@ class Snake extends React.Component {
 }
 Snake.defaultProps = {//export default propsow
     //@TODO it should be checked if bigger than 5 
-    boardDimension: 10
+    boardDimension: 10,
+    startGameTickTime: 1000
 }
 
 export default Snake
